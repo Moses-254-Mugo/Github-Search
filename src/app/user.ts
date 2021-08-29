5e745f2c8a534d58a3742faf74f20ae7b25b7ffb
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-29 09:48:58 +0300

    Modify class
    user

--- src/app/user.ts
+++ src/app/user.ts
@@ -4,13 +4,19 @@ export class User {
      followers : number;
      following : number;
      bio : string;
+     email: string;
+     location: string;
+     created_at: Date
 
-     constructor(login: string, avatar_url: string, followers: number, following: number, bio : string){
+     constructor(login: string, avatar_url: string, followers: number, following: number, bio : string, email: string,location: string,  created_at: Date){
          this.login =login;
          this.avatar_url = avatar_url;
          this.followers = followers;
          this.following = following;
          this.bio = bio;
+         this.email = email;
+         this.location = location;
+         this.created_at = created_at;
      }
 
 
