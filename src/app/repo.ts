5fc441090cd6410498844574ad2010039d703d94
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-08-29 08:51:04 +0300

    Add properties to class repo

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -2,12 +2,23 @@ export class Repo {
 
         avatar_url : string;
         name: string;
+        description: string;
+        language: string;
+        watchers:string;
+        forks: string;
+        default_branch: string;
+
 
    
-        constructor(avatar_url: string, name: string){
+        constructor(avatar_url: string, name: string, description: string,language: string, watchers:string,forks: string, default_branch: string){
     
             this.avatar_url = avatar_url;
             this.name = name;
+            this.description = description;
+            this.language = language;
+            this.watchers = watchers;
+            this.forks = forks;
+            this.default_branch = default_branch;
     
     
         }
